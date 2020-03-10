092103ca367a30f908f36a72e5840cd3cbe325ab
MaximG99 <60999738+MaximG99@users.noreply.github.com>
2020-03-10 09:47:51 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from weather import weather_by_city
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    page_title = "Прогноз погоды"
+    weather = weather_by_city("Moscow,Russia")
+    return render_template("index.html", page_title=page_title, wether_text=wether_text) 
+    
+    
+        
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
