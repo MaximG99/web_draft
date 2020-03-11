65a22fe4dde13cfebb9f9dcd5ef34db71265c33d
MaximG99 <60999738+MaximG99@users.noreply.github.com>
2020-03-11 15:26:56 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,28 @@
+import requests
+
+def weather_by_city(city_name):
+    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    params = {
+        "key":"64837ac3976646e3ae444244200203",
+        "q": city_name,
+        "format": "json",
+        "num_of_days": 1,
+        "lang": "ru"
+    }
+    try:
+       result = requests.get(weather_url,params=params)
+       result.raise_for_status()
+       weather = result.json()
+       if "date" in weather:
+           if"current_condition" in weather["date"]:
+              try:
+                  return weather["date"]["current_condition"][0]
+              except(IndexError,TypeError):
+                  return False
+    except(requests,RecuestExcept,ValueError):
+        print("Сетевая ошибка")
+        return False
+    return False
+if __name__== "__main__":
+    w = weather_by_city("Moscow,Russia")
+    print(w)    
