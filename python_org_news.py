6a6eb94cd5a87ad0930f001f854a0a5404e3730c
MaximG99 <60999738+MaximG99@users.noreply.github.com>
2020-03-16 16:27:17 +0300

    Add files via upload

--- python_org_news.py
+++ python_org_news.py
@@ -1,23 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_html(url):
+def get_htmlget_html(url):
     try:
-        result = requests.get(url)
+        result =  requests.get(url)
         result.raise_for_status()
         return result.text
-    except(requests.RequestException,ValueError):
+    except(requests.RequestException, ValueError):
         print("Сетевая ошибка")
         return False
 
 def get_python_news(html):
-    soup = BeautifulSoup(html,"html.parser")
-    all_news = soup.find("ul", class_="list-recent-post" )
+    soup =  BeautifulSoup(html, "html.parser")
+    all_news =  soup.find("ul", class_= "list-recent-post" )
     print(all_news)
 
 
-if __name__ == "__main__":
-        html = get_html("https://www.python.org/blogs/")
+if __name__ ==  "__main__":
+        html = get_htmlget_html("https://www.python.org/blogs/")
         if html:
-            get_python_news(html)
-           
+            get_python_newsget_python_news (html)
